6e41691388e6850d721a2d95b64f25fca9c6d9fc
Athilakshmi-ms <athilakshmi.ms@gmail.com>
2023-06-07 09:18:35 +0200

    introduction challenges

---
+++ script.js
@@ -0,0 +1,14 @@
+//Display the document title in the console
+
+console.log(document.title);
+
+//Change the document title to Modifying the DOM
+
+document.title = 'Modifying the DOM';
+
+//Change the background color of the body to hot pink (#FF69B4).
+
+document.querySelector('body').style.backgroundColor = '#FF69B4';
+//document.querySelector('body').style.backgroundColor = 'rgb(33,55,77)';
+
+//display all children elements of the <body>
