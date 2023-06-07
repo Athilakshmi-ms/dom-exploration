09d56920be44e24c25cc534cd8daaba3f981d9bf
Athilakshmi-ms <athilakshmi.ms@gmail.com>
2023-06-07 18:48:19 +0200

    intro exercises completed

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ document.title = 'Modifying the DOM';
 
 //Change the background color of the body to hot pink (#FF69B4).
 
-document.querySelector('body').style.backgroundColor = '#FF69B4';
-//document.querySelector('body').style.backgroundColor = 'rgb(33,55,77)';
+//document.body.style.backgroundColor = '#FF69B4';
+document.querySelector('body').style.backgroundColor = 'rgb(230,235,250)';
 
 //display all children elements of the <body>
+const childElements = document.body.children;
+let displayElements = "";
+for(let i = 0; i < childElements.length; i++){
+    displayElements += childElements[i].tagName + "<br>";
+}
+document.getElementById("displayElements").innerHTML = displayElements;
