fcba2682696040828e5105474c8f67875b0c2a16
Athilakshmi-ms <athilakshmi.ms@gmail.com>
2023-06-08 14:22:34 +0200

    Learning selectors

---
+++ Selectors/script.js
@@ -0,0 +1,15 @@
+//Add a title attribute to every element that has the important class,
+
+let important = document.getElementsByClassName("important");
+for(let i=0; i<important.length; i++){
+important[i].setAttribute("title","This is an important item");
+}
+//Select all the img tags and loop through them. If they have no important class, turn their display property to none
+
+
+// take the img elements and put them in a let 
+//create a for loop and check each element for class
+//we add a if else clause to give the condition
+//if this is true  === then this happens
+
+
