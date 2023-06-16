e6904ca6cebb4b2d763069e312b0b9f1f9b3eb0e
Athilakshmi-ms <athilakshmi.ms@gmail.com>
2023-06-16 12:33:26 +0200

    collection js codes update

--- Collection-project/assets/script.js
+++ Collection-project/assets/script.js
@@ -70,6 +70,75 @@ const collection = [{
     picture: 'images/Rottweiler.jpg'
 }];
 console.log(collection);
+
+for (let i = 0; i < collection.length; i++) {
+    let container = document.getElementById('container');
+    let imageContainer = document.createElement('div');
+    let image = document.createElement('img');
+    let bottomContainer = document.createElement('div');
+    let categoryContainer = document.createElement('div');
+    let category = document.createElement('p');
+    let categoryValue = document.createElement('p');
+    let content = document.createElement('div');
+    let heading = document.createElement('h3');
+    let subTitle = document.createElement('p');
+    let description = document.createElement('p');
+
+    imageContainer.appendChild(image);
+    categoryContainer.append(category);
+    categoryContainer.append(categoryValue);
+    content.append(heading);
+    content.append(subTitle);
+    content.append(description);
+    bottomContainer.append(categoryContainer);
+    bottomContainer.append(content);
+    container.append(imageContainer);
+    container.append(bottomContainer);
+
+    image.src = collection[i].picture;
+    category.innerText = collection[i].years;
+    categoryValue.innerText = collection[i].lifespan;
+    heading.innerText = collection[i].name;
+    subTitle.innerText = collection[i].native;
+    description.innerText = "The dog is a domesticated descendant of the wolf.  Dogs were the first species-be domesticated by hunter-gatherers 15,000 years ago before the development of agriculture.";
+
+    //container for each object should be
+    container.style.backgroundColor = "#e6e2e2";
+    container.style.borderRadius = "5px";
+    container.style.fontSize = "0.8rem";
+    container.style.textAlign = "left";
+    container.style.width = "180px";
+    container.style.height = "400px";
+    container.style.border = "1px solid black";
+
+    //create a container
+    image.style.objectFit = "cover";
+    image.style.width = "180px";
+    image.style.height = "100px";
+
+    //category content
+    categoryContainer.style.display = "flex";
+    categoryValue.style.marginLeft = "0.5rem";
+    categoryValue.style.border = "1px solid rgb(110, 183, 204)";
+    categoryValue.style.padding = "3px"
+    categoryValue.style.borderRadius = "5px"
+    categoryValue.style.fontSize = "10px"
+    categoryValue.style.backgroundColor = "rgb(110, 183, 204)"
+    category.style.padding = "3px"
+    category.style.fontSize = "10px"
+    category.style.border = "1px solid #e1e10f";
+    category.style.backgroundColor = "#e1e10f"
+    category.style.borderRadius = "5px"
+
+    //heading content
+    heading.style.fontSize = "1rem";
+    heading.style.paddingBottom = "0px";
+    subTitle.style.color = "gray";
+    subTitle.style.paddingTop = "0px";
+    bottomContainer.style.padding = "10px";
+}
+/*
+
 let sampleContainer = {
     name: 'Rajapalayam',
     native: 'Tamil Nadu',
@@ -78,70 +147,23 @@ let sampleContainer = {
     picture: 'assets/images/Rajapalayam.jpg'
 };
 
-let container = document.getElementById('container');
-let imageContainer = document.createElement('div');
-let image = document.createElement('img');
-let bottomContainer = document.createElement('div');
-let categoryContainer = document.createElement('div');
-let category = document.createElement('p');
-let categoryValue = document.createElement('p');
-let content = document.createElement('div');
-let heading = document.createElement('h3');
-let subTitle = document.createElement('p');
-let description = document.createElement('p');
-
-imageContainer.appendChild(image);
-categoryContainer.append(category);
-categoryContainer.append(categoryValue);
-content.append(heading);
-content.append(subTitle);
-content.append(description);
-bottomContainer.append(categoryContainer);
-bottomContainer.append(content);
-container.append(imageContainer);
-container.append(bottomContainer);
-
-//container for each object should be
-container.style.backgroundColor = "#e6e2e2";
-container.style.fontSize = "0.8rem";
-container.style.textAlign = "left";
-container.style.width = "180px";
-container.style.height = "400px";
-container.style.border = "1px solid black";
-
-
-//create a container
 image.src = sampleContainer.picture;
-image.style.objectFit = "cover";
-image.style.width = "180px";
-image.style.height = "100px";
-
-//category content
 category.innerText = sampleContainer.years;
 categoryValue.innerText = sampleContainer.lifespan;
-categoryContainer.style.display = "flex";
-categoryValue.style.marginLeft = "0.5rem";
-categoryValue.style.border = "1px solid rgb(110, 183, 204)";
-categoryValue.style.padding = "3px"
-categoryValue.style.borderRadius = "5px"
-categoryValue.style.fontSize = "10px"
-categoryValue.style.backgroundColor = "rgb(110, 183, 204)"
-category.style.padding = "3px"
-category.style.fontSize = "10px"
-category.style.border = "1px solid #e1e10f";
-category.style.backgroundColor = "#e1e10f"
-category.style.borderRadius = "5px"
-
-//heading content
 heading.innerText = sampleContainer.name;
-heading.style.fontSize = "1rem";
-heading.style.paddingBottom = "0px";
-subTitle.style.color = "gray";
-subTitle.style.paddingTop = "0px";
 subTitle.innerText = sampleContainer.native;
 description.innerText = "The dog is a domesticated descendant of the wolf.  Dogs were the first species-be domesticated by hunter-gatherers 15,000 years ago before the development of agriculture.";
-bottomContainer.style.padding = "10px";
+*/
 
+/*collection.forEach((index) => {
+    image.src = collection[index].picture;
+    category.innerText = collection[index].years;
+    categoryValue.innerText = collection[index].lifespan;
+    heading.innerText = collection[index].name;
+    subTitle.innerText = collection[index].native;
+    description.innerText = "The dog is a domesticated descendant of the wolf.  Dogs were the first species-be domesticated by hunter-gatherers 15,000 years ago before the development of agriculture.";
+});
+*/
 
 
 
